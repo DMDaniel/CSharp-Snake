649dd630094880321d00e06df12472c9dcb78a54
Daniel Mender <dmdsplanet@gmail.com>
2013-04-05 18:38:56 +0200

    New file (Remote UC)

---
+++ snake1.0.2.1/snake1.0.2/View/RemoteGameUserControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace snake1._0._2.View
+{
+    public partial class RemoteGameUserControl : UserControl
+    {
+        public RemoteGameUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
